b8058b76c2c26aa9699f4cca14b1b1a2cbe6fa7f
wangs <857707645@qq.com>
2025-08-08 18:01:55 +0800

    21ushushush

--- linux_kernel/platform/ws_pdev.c
+++ linux_kernel/platform/ws_pdev.c
@@ -5,7 +5,7 @@
 int ws_pdev_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 {
     pr_info("Probing device %s with ID %04x:%04x\n", pci_name(pdev), pdev->vendor, pdev->device);
-
+    return 0;
 }
 void ws_pdev_remove(struct pci_dev *pdev)
 {
