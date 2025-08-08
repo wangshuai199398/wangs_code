0737cb48a21715edfbc5967fc08c1e7705ca1e4a
wangs <857707645@qq.com>
2025-08-08 16:15:56 +0800

    21ushush

--- platform/ws_init.c
+++ platform/ws_init.c
@@ -2,10 +2,15 @@
 
 #include <ws_init.h>
 
-int ws_init2(struct ws_pci *ws_pdrv)
+int ws_init2(struct ws_pci_driver *ws_pdrv)
 {
     int ret;
+    ret = 0;
     pr_info("Driver %s Init\n", THIS_MODULE->name);
-
+    return ret;
 }
 
+void ws_exit2(struct ws_pci_driver *ws_pdrv)
+{
+    pr_info("Driver %s exit\n", THIS_MODULE->name);
+}
