8fc23f94f3b7d0eb853f3ff4acc4de32d51bc040
wangs <857707645@qq.com>
2025-08-11 10:45:13 +0800

    21ushushush

--- linux_kernel/platform/ws_ndev.c
+++ linux_kernel/platform/ws_ndev.c
@@ -5,7 +5,7 @@
 int ws_aux_eth_probe(struct auxiliary_device *adev, const struct auxiliary_device_id *id)
 {
     pr_info("Probing auxiliary device %s\n", adev->dev.kobj.name);
-
+    return 0;
 }
 
 void ws_aux_eth_remove(struct auxiliary_device *adev)
