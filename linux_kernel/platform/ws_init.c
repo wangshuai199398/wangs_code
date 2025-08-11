4fd4a32637db5ffa93b64b6e13676e7df743813f
wangs <857707645@qq.com>
2025-08-11 08:57:18 +0800

    21ushushush

--- linux_kernel/platform/ws_init.c
+++ linux_kernel/platform/ws_init.c
@@ -8,13 +8,17 @@ int ws_init2(struct ws_pci_driver *ws_pdrv)
 {
     int ret;
     pr_info("Driver %s Init\n", THIS_MODULE->name);
-    pr_info("Driver %s Init\n", WS_HW_NAME);
+
     ws_debugfs_init();
+
+
+    
     return ret;
 }
 
 void ws_exit2(struct ws_pci_driver *ws_pdrv)
 {
-    ws_pdrv->pdrv.name = THIS_MODULE->name;
     pr_info("Driver %s exit\n", THIS_MODULE->name);
+
+    ws_debugfs_exit();
 }
